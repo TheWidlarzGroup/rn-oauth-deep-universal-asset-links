c08fff94af1dcca57b950c75868663e8f0fc53f1
Daniel Grychtoł <grychtol.daniel@gmail.com>
2021-11-12 13:50:27 +0100

    Update <MainScreen />

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+import {logInWithGoogle} from '../services/oauth';
 
 const Main = () => {
+  const onPress = () => {
+    logInWithGoogle();
+  };
+
   return (
     <View style={styles.container}>
       <Text>Main screen</Text>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button} onPress={onPress}>
+          <Text style={styles.buttonText}>Log In with Google</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -14,6 +24,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 8
+  },
+  button: {
+    backgroundColor: '#79dff4',
+    padding: 12,
+    paddingHorizontal: 16,
+    borderRadius: 2,
+    elevation: 2,
+    shadowOpacity: 0.1,
+    shadowOffset: {
+      height: 2,
+      width: 0
+    },
+    shadowRadius: 0.2
+  },
+  buttonText: {
+    color: '#fefefe',
+    fontWeight: 'bold'
   }
 });
 
