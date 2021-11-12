802b8f699923ddf23480a961242c1d2bf9450547
Daniel Grychtoł <grychtol.daniel@gmail.com>
2021-11-12 13:49:51 +0100

    Add prototype of acc conf

--- src/screens/AccountConfirm.tsx
+++ src/screens/AccountConfirm.tsx
@@ -1,7 +1,26 @@
+import {RouteProp, useRoute} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {useEffect} from 'react';
+import {StyleSheet, View, Text, Alert} from 'react-native';
+import {
+  MainNavigationParams,
+  MainNavigationRoutes
+} from '../types/MainNavigation';
+
+type Route = RouteProp<
+  MainNavigationParams,
+  MainNavigationRoutes.ACCOUNT_CONFIRM
+>;
 
 const AccountConfirm = () => {
+  const {
+    params: {token}
+  } = useRoute<Route>() || {token: ''};
+
+  useEffect(() => {
+    Alert.alert('Token:', token);
+  }, [token]);
+
   return (
     <View style={styles.container}>
       <Text>Account confirm screen</Text>
