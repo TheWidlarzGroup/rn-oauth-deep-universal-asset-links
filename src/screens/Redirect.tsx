0a4127698f78bb25003c0f9a8e152711c8414bcd
Daniel Grychtoł <grychtol.daniel@gmail.com>
2021-11-12 13:50:16 +0100

    Updare <RedirectScreen />

--- src/screens/Redirect.tsx
+++ src/screens/Redirect.tsx
@@ -1,7 +1,23 @@
+import {RouteProp, useRoute} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {useEffect} from 'react';
+import {StyleSheet, View, Text, Alert} from 'react-native';
+import {
+  MainNavigationParams,
+  MainNavigationRoutes
+} from '../types/MainNavigation';
+
+type Route = RouteProp<MainNavigationParams, MainNavigationRoutes.REDIRECT>;
 
 const Redirect = () => {
+  const {
+    params: {token}
+  } = useRoute<Route>() || {params: {token: ''}};
+
+  useEffect(() => {
+    Alert.alert('Token:', token);
+  }, [token]);
+
   return (
     <View style={styles.container}>
       <Text>Redirect screen</Text>
